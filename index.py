599a8db48db2aedc608f19ed171742f2beec3bdd
nithish <nithish98raja@gmail.com>
2021-06-26 19:28:24 +0530

    add logic for displaying lifetimes plot

--- index.py
+++ index.py
@@ -8,5 +8,8 @@ import matplotlib.pyplot as plt
 
 # Local dependencies
 from src.averages import displayAverages
+from src.lifetimes import displayLifetimes
 
+# Call function to display plots
 displayAverages()
+displayLifetimes()
---
+++ src/lifetimes.py
@@ -0,0 +1,56 @@
+#
+# Test file
+#
+#
+
+# Dependencies
+import os
+import csv
+import matplotlib.pyplot as plt
+
+def displayLifetimes():
+    # Initialise number of iterations
+    numberOfIterations = 100
+
+    # Initialise object to store data
+    data = {}
+
+    # Iterate over number of days
+    for i in range(numberOfIterations):
+        # Read player config file
+        file = open(os.path.join("logging", "day"+str(i), "playerConfig"))
+        reader = csv.reader(file)
+        # Iterate over each row
+        for row in reader:
+            # Check if player already exists in data dict
+            if not row[0] in data.keys():
+                data[row[0]] = {
+                    "lifetime": [],
+                    "movementLimit": [],
+                    "visionLimit": [],
+                    "rechargeDuration": []
+                }
+            # Update player info on data dict
+            data[row[0]]["lifetime"].append(i)
+            data[row[0]]["movementLimit"].append(float(row[1]))
+            data[row[0]]["visionLimit"].append(float(row[2]))
+            data[row[0]]["rechargeDuration"].append(float(row[3]))
+
+    # Iterate over attributes and display lifetime graph
+    for attribute in ["movementLimit", "visionLimit", "rechargeDuration"]:
+        plt.figure()
+        # Iterate over all players
+        for player in data.keys():
+            plt.plot(data[player]["lifetime"], data[player][attribute], label=player)
+
+        plt.xlabel("lifetime")
+        plt.ylabel(attribute)
+        plt.legend(bbox_to_anchor=(1, 1), loc='upper left')
+        plt.tight_layout()
+        plt.show()
+        plt.close()
+
+# Check if module is used as script
+if __name__ == "__main__":
+    # Call function to display lifetimes plot
+    displayLifetimes()
